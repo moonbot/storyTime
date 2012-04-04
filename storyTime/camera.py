d56f1ff22e95ee227d3e858dd8dffffd4fe2b9c8
Nolan Baker <nolan.lee.baker@gmail.com>
2012-04-03 20:22:56 -0500

    camera recording works

--- storyTime/camera.py
+++ storyTime/camera.py
@@ -12,6 +12,9 @@ import shutil
 import tempfile
 import threading
 import time
+import pdb
+import sys
+import subprocess
 
 LOG = logging.getLogger('storyTime.camera')
 
@@ -20,37 +23,15 @@ class VideoRecording(threading.Thread):
         threading.Thread.__init__(self)
         self.isRecording = False
         self.hasRecording = False
-        self._camera = None
-        self._writer = None
-        self._tempFile = None
         self.fps = 24.0
         self.prevTime = 0
         self.duration = 0
+        self.camera = cv.CaptureFromCAM(-1)
             
     def __del__(self):
         if self.isRecording:
             self.stop()
-        del(self._camera)
-
-    @property
-    def tempFile(self):
-        if self._tempFile is None:
-            fd, self._tempFile = tempfile.mkstemp('_cameraCapture.mov')
-            LOG.debug(self._tempFile)
-            os.close(fd)
-        return self._tempFile
-       
-    @property
-    def camera(self):
-        if self._camera is None:
-            self._camera = cv.CaptureFromCAM(-1)
-        return self._camera
-                
-    @property
-    def writer(self):
-        if self._writer is None:
-            self._writer = 1#cv.CreateVideoWriter(self.tempFile, cv.CV_FOURCC('H','2','6','4'), self.fps, (480,320), 1)
-        return self._writer
+        del(self.camera)
         
     def record(self):
         if self.isRecording:
@@ -68,57 +49,34 @@ class VideoRecording(threading.Thread):
             
     def run(self):
         # thread function called by self.start()
+        index = 0
         while self.isRecording:
+            index += 1
             elapsedTime = time.time() - self.prevTime
             self.duration += elapsedTime
             if elapsedTime > 1.0 / self.fps:
-                image = cv.RetrieveFrame(self.camera)
+                image = cv.QueryFrame(self.camera)
                 if image is not None:
-                    print("my")
-                    # cv.WriteFrame(self.writer, image)
-                print("done")
-        
+                    cv.SaveImage(os.path.join(tempfile.gettempdir(), 'cameraExport.%06d.jpg' % index), image)
+                        
     def save(self, filename):
-        if not self.hasRecording:
-            LOG.warning('Nothing to save. No recording has been created yet')
-            return
-        filename = '{0}.wav'.format(os.path.splitext(filename)[0])
-        self.filename = filename
-        dir_ = os.path.dirname(filename)
-        if not os.path.isdir(dir_):
-            try:
-                os.makedirs(dir_)
-            except OSError as e:
-                LOG.warning('Could not save to file, cannot create directory: {0}'.format(dir_))
-                return
-        shutil.copy2(self.tempFile, filename)
-        return self.filename
-
-
+        FFMPEG = 'ffmpeg.exe' if sys.platform == 'win32' else 'ffmpeg'
+        args = [
+            FFMPEG,
+            '-y',
+            '-f', 'image2',
+            '-i', os.path.join(tempfile.gettempdir(), 'cameraExport.%06d.jpg'),
+            '-r', self.fps,
+            '-vcodec', 'libx264',
+            '-g', '12',
+            '-t', float(self.duration) / self.fps,
+            filename,
+        ]
+        args = [str(a) for a in args]
+        LOG.debug('ffmpeg command:\n {0}'.format(' '.join(args)))
+        subprocess.Popen(args)
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+v = VideoRecording()
+v.record()
+pdb.set_trace()
+ 
